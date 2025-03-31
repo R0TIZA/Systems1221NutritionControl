edd5d5d5e0e5a12dc4d7243a384ce2928192f558
R0TIZA <loltawon@mail.ru>
2025-03-31 16:36:18 +0300

    Создал пустой Spring Boot проект.

---
+++ src/main/java/ru/rotiza/systems1221nutritioncontrol/Systems1221NutritionControlApplication.java
@@ -0,0 +1,13 @@
+package ru.rotiza.systems1221nutritioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Systems1221NutritionControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Systems1221NutritionControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rotiza/systems1221nutritioncontrol/Systems1221NutritionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rotiza.systems1221nutritioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Systems1221NutritionControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
